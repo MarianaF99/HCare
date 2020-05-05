1c72f935c2900951a6501a3aaccba2b14d962049
Andrea Fernandes <andreaisabelfreitas@gmail.com>
2020-05-05 19:45:50 +0100

    Implementação do localstorage
    +login
    +registro
    +validate

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,65 @@
 function ProcessLoginForm(e) {
 
-    //e.preventDefault()
+    let email = document.getElementById("loginEmail").value;
+    let password = document.getElementById("loginPassword").value;
 
-    //alert("Login com sucesso")
-    return true
+    let result = ValidateLogin(email, password);
+
+    if (result === true) {
+        alert("Login com sucesso")
+    } else {
+        alert("Please register first")
+    }
+
+    return result
+}
+
+function ProcessRegisterForm(e) {
+
+    e.preventDefault()
+    var users = GetAllUsersFromLocalStorage()
+
+    let user = {
+        email: document.getElementById("registerEmail").value,
+        password: document.getElementById("registerPassword").value
+    }
+
+    users.push(user);
+
+    localStorage.setItem("users", JSON.stringify(users));
+
+
+    alert("Registo com sucesso")
+
+    return false
+}
+
+function GetAllUsersFromLocalStorage() {
+    var users = JSON.parse(localStorage.getItem("users") || "[]");
+
+    return users;
+}
+
+function PrintAllUsersFromLocalStorage() {
+    var users = GetAllUsersFromLocalStorage()
+
+    users.forEach(function(user, i) {
+        console.log(i + " - " + user.email + " %% " + user.password)
+    });
+}
+
+function ValidateLogin(email, password) {
+
+    PrintAllUsersFromLocalStorage()
+    var users = GetAllUsersFromLocalStorage()
+    let result = false;
+
+    users.forEach(function(user, i) {
+
+        if (user.email === email && user.password === password) {
+            result = true;
+        }
+    });
+
+    return result;
 }
